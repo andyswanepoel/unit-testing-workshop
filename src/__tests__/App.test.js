541acd78d9f92b7a3936699d131369bbcdc1c625
andyswanepoel <34516986+andyswanepoel@users.noreply.github.com>
2022-03-03 14:19:30 -0500

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../App";
 
 global.fetch = jest.fn();
@@ -100,15 +101,31 @@ describe("User list sorting", () => {
     fetch.mockClear();
   });
 
+  test("button should be disabled if no users found", async () => {
+    fetch.mockImplementationOnce(() =>
+      Promise.resolve({ json: () => Promise.resolve(mockEmptyResponse) })
+    );
+    render(<App />);
+    expect(await screen.findByText(/no users found/i)).toBeInTheDocument();
+    const sortBtn = screen.getByRole("button", { name: /sort by name/i });
+    expect(sortBtn).toBeDisabled();
+  });
+
   test("should be sorted in ascending order when sort button is clicked", async () => {
     fetch.mockImplementationOnce(() =>
       Promise.resolve({ json: () => Promise.resolve(mockSuccessResponse) })
     );
     render(<App />);
 
+    const sortedUserNames = mockSuccessResponse.results
+      .map((u) => u.name.first + " " + u.name.last)
+      .sort();
+    const sortBtn = screen.getByRole("button", { name: /sort by name/i });
+    userEvent.click(sortBtn);
+
     const users = await screen.findAllByRole("heading", { level: 2 });
     const userNames = users.map((user) => user.textContent);
-    const sortedUserNames = userNames.map((x) => x).sort();
+
     expect(userNames).toEqual(sortedUserNames);
   });
 
@@ -118,12 +135,18 @@ describe("User list sorting", () => {
     );
     render(<App />);
 
-    const users = await screen.findAllByRole("heading", { level: 2 });
-    const userNames = users.map((userName) => userName.textContent);
-    const sortedUserNames = userNames
-      .map((x) => x)
+    const sortedUserNames = mockSuccessResponse.results
+      .map((u) => u.name.first + " " + u.name.last)
       .sort()
       .reverse();
+
+    const sortBtn = screen.getByRole("button", { name: /sort by name/i });
+    userEvent.click(sortBtn);
+    userEvent.click(sortBtn);
+
+    const users = await screen.findAllByRole("heading", { level: 2 });
+    const userNames = users.map((userName) => userName.textContent);
+
     expect(userNames).toEqual(sortedUserNames);
   });
 });
