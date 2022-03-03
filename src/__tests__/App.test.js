0ddaee31cbec2adbe411d9e0c0a115ea00c91329
andyswanepoel <34516986+andyswanepoel@users.noreply.github.com>
2022-03-03 17:07:18 -0500

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -101,16 +101,6 @@ describe("User list sorting", () => {
     fetch.mockClear();
   });
 
-  test("button should be disabled if no users found", async () => {
-    fetch.mockImplementationOnce(() =>
-      Promise.resolve({ json: () => Promise.resolve(mockEmptyResponse) })
-    );
-    render(<App />);
-    expect(await screen.findByText(/no users found/i)).toBeInTheDocument();
-    const sortBtn = screen.getByRole("button", { name: /sort by name/i });
-    expect(sortBtn).toBeDisabled();
-  });
-
   test("should be sorted in ascending order when sort button is clicked", async () => {
     fetch.mockImplementationOnce(() =>
       Promise.resolve({ json: () => Promise.resolve(mockSuccessResponse) })
